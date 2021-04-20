f7ff67cf1e8e2c351a7b714d6457d19cfea91ff8
Gytzum <gytzum@gmail.com>
2021-04-20 17:18:20 +0300

    Time

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,11 +1,35 @@
 function calcTimeTillDate(date) {
-    const targetDate = '2022-'+ date
-    return {
-        days: 45,
-        hours: 7,
-        minutes: 35,
-        seconds: 9
+    const clock = new Date();
+    let year = clock.getFullYear()
+
+    let yearTarget = `${year}- ${date}` //gaunam 2021-03-15 14:30:00
+    const yearDateObject = new Date(yearTarget) //gaunam datos objekta
+    const currMiliseconds = yearDateObject.getTime() //suzinom kiek milisekundziu prabego nuo 1970
+    const nowInMiliseconds = Date.now();
+
+    if(currMiliseconds < nowInMiliseconds){
+        year++;
+        yearTarget = `${year}-${date}`;
+        yearDateObject = new Date(yearTarget);
+        currMiliseconds = yearDateObject.getTime();
     }
+
+    const timeLeftInMiliseconds = currMiliseconds - nowInMiliseconds;
+    let timeLeftInSeconds = Math.round(timeLeftInMiliseconds/ 1000);
+    // const seconds = timeLeftInMiliseconds%60;
+    // const minutes = (timeLeftInMiliseconds - seconds) / 60 % 60;
+    // const hours = (timeLeftInSeconds - seconds - minutes * 60)/360 % 24;
+    // const days = (timeLeftInSeconds - seconds - minutes *60 - houes * 3600) / 86400;
+
+    const days = Math.floor(timeLeftInSeconds / 86400);
+    timeLeftInSeconds -= days*86400
+    const hours = Math.floor(timeLeftInSeconds/3600);
+    timeLeftInSeconds -= seconds*3600;
+    const minutes = Math.floor(timeLeftInSeconds / 60)
+    timeLeftInSeconds -= minutes * 60
+    const seconds = timeLeftInSeconds % 60;
+
+    return {days, hours, minutes, seconds}
 }
 
 function formatNumber(number){
