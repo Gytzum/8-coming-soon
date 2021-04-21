ce778c1ed4390b781a16c859f376f0f965443fc9
Gytzum <gytzum@gmail.com>
2021-04-21 16:29:11 +0300

    icons

--- js/data/socialsData.js
+++ js/data/socialsData.js
@@ -2,36 +2,36 @@ const socialsData = [
     {
         icon: 'facebook',
         href: '#',
-    }
+    },
 
     {
         icon: 'twitter',
         href: '#',
-    }
+    },
 
     {
         icon: 'dribbble',
         href: '#',
-    }
+    },
 
     {
         icon: 'tumblr',
         href: '#',
-    }
+    },
 
     {
         icon: 'linkedin',
         href: '#',
-    }
+    },
     {
         icon: 'instagram',
         href: '#',
-    }
+    },
 
     {
         icon: 'rss',
         href: '#',
-    }
+    },
 ];
 
 export{ socialsData }
